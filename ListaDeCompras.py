7171d383d919e312374426fa5918972f692c8da2
Laura Vidal <lauravidal@outlook.com>
2024-02-07 22:57:27 -0300

    adicionando o deletar com a integracao do .txt no listadecompras

--- ListaDeCompras.py
+++ ListaDeCompras.py
@@ -23,30 +23,53 @@ def listaCompras():
     botao_lista_compras = Button(janela_lista_compras, text="Adicionar Produto", width=20, command=lambda: adicionar(entrada_lista_compras, listbox_lista_compras))
     botao_lista_compras.pack(padx=10, pady=5)
 
-    botao_lista_compras = Button(janela_lista_compras, text="Deletar Produto", width=20, command=deletar)
+    botao_lista_compras = Button(janela_lista_compras, text="Deletar Produto", width=20, command=lambda: deletar(listbox_lista_compras))
     botao_lista_compras.pack(padx=10, pady=5)
+
+    leitura_txt(listbox_lista_compras)
 
     janela_lista_compras.mainloop()
 
 def adicionar(entrada, listbox):
     produto = entrada.get()
     if produto != "" and produto != " ":
-        listbox.insert(END, produto)
-        with open("produtos.txt", 'a', encoding="utf-8") as file:
-            file.write(produto)
+        item = f"{produto}\n"
+        with open("listacompras.txt", 'a', encoding="utf-8") as file:
+            file.write(item)
+        listbox.insert(END, item)
         entrada.delete(0, END)
     else:
         tkinter.messagebox.showwarning(title="Erro!", message="Escreva um produto na barra de entrada")
 
+# def deletar(listbox):
+#     try:
+#         index_compras = listbox.curselection()[0]
+#         listbox.delete(index_compras)
+#     except:
+#         tkinter.messagebox.showwarning(title="Erro!", message="Selecione um produto com seu cursor")
+        
 def deletar(listbox):
     try:
-        index_compras = listbox.curselection()[0]
-        listbox.delete(index_compras)
-    except:
-        tkinter.messagebox.showwarning(title="Erro!", message="Selecione um produto com seu cursor")
+        index_lista_compras = listbox.curselection()[0]
+        item_selecionado = listbox.get(index_lista_compras) + "\n"
+        listbox.delete(index_lista_compras)
 
-def salvar():
-    return
+        with open("listacompras.txt", 'r', encoding="utf-8") as file:
+            linhas = file.readlines()
+        
+        with open("listacompras.txt", 'w', encoding="utf-8") as file:
+            for linha in linhas:
+                if linha != item_selecionado:
+                    file.write(linha)
 
-def carregar():
-    return
+    except IndexError:
+        tkinter.messagebox.showwarning(title="Erro!", message="Selecione um produto com o cursor.")
+        
+def leitura_txt(listbox):
+    try:
+        with open("listacompras.txt", 'r', encoding="utf-8") as file:
+            linhas = file.readlines()
+            for linha in linhas:
+                listbox.insert(END, linha.strip())
+    except FileNotFoundError:
+        pass
